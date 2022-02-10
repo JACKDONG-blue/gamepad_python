8d784f664bd1bf8e49101e77ec09ad27db8b0cab
Shunkai Xing <xingshunkai@qq.com>
2022-02-11 02:46:41 +0800

    完成多线程版本

--- src/gamepad.py
+++ src/gamepad.py
@@ -1,14 +1,40 @@
 '''
 游戏手柄
 '''
-from re import S
-from threading import Event
+import time
+from threading import Event, Thread
 from inputs import get_gamepad
+from sympy import EX
 
 from sensor.sensor import Sensor
 from sensor.button import Button
 from utils.logger_interface import LoggerInterface
+from utils.thread import KillableThread
 
+class EventListenerThread(Thread):
+	'''事件监听线程'''
+	def __init__(self, gamepad):
+		self.stop_event = Event()
+		self.gamepad = gamepad
+		super().__init__()
+		
+	def run(self):
+		while True:
+			print("...")
+			try:
+				if self.stop_event.is_set():
+					print("退出")
+					break
+				# 事件监听
+				self.gamepad.event_listener()
+				time.sleep(0.001)
+			except Exception as e:
+				print(e)
+	def stop(self):
+		'''停止'''
+		self.stop_event.set()
+		print(f"self.stop_event.set() = {self.stop_event.is_set()}")
+	
 class GamePad(LoggerInterface):
 	'''游戏手柄'''
 	def __init__(self, name="Gamepad", logging_level=None) -> None:
@@ -20,8 +46,9 @@ class GamePad(LoggerInterface):
 		# 初始化事件-传感器映射字典
 		self.init_event_sensor_map()
 		# 多线程参数
-		self.exit_event = Event()
-	
+		# self.exit_event = Event()
+		# 事件监听线程
+		self.event_listener_thread = None
 	def init_sensor(self):
 		'''传感器初始化'''
 		# 根据手柄布局，配置传感器
@@ -41,28 +68,45 @@ class GamePad(LoggerInterface):
 					self.event_sensor_map[sensor.event_code] = [sensor,]
 				
 		# print(self.event_sensor_map)
-		
-	def event_listener(self):
-		'''事件监听器'''
-		while True:
-			# 退出
-			if self.exit_event.is_set():
-				return
-			# 遍历事件
-			events =  get_gamepad()
-			for event in events:
-				# self.logger.info("事件捕获")
-				# self.logger.info(f"设备名: {event.device} 触发时间: {event.timestamp}")
-				# 数据类型 |字符串|字符串|整数|
-				# self.logger.info(f"事件类型: {event.ev_type} | 事件代码: {event.code} | 事件状态: {event.state}")  # 字符串
-				# 判断事件是否在映射地图里
-				if event.code not in self.event_sensor_map:
-					# self.logger.info(f"未知事件代码: {event.code}")
-					continue
-				# 获取该传感器
-				sensor_list = self.event_sensor_map[event.code]
-				# 执行回调函数
-				for sensor in sensor_list:
-					# self.logger.info(f"响应传感器: {sensor.name}")
-					sensor.event_handler(event)
 	
+	def event_listener(self):
+		'''事件监听'''
+		# 遍历事件(这个函数是阻塞的)
+		# 如果没有事件发生，则会一直卡在这里
+		events =  get_gamepad()
+		for event in events:
+			# self.logger.info("事件捕获")
+			# self.logger.info(f"设备名: {event.device} 触发时间: {event.timestamp}")
+			# 数据类型 |字符串|字符串|整数|
+			# self.logger.info(f"事件类型: {event.ev_type} | 事件代码: {event.code} | 事件状态: {event.state}")  # 字符串
+			# 判断事件是否在映射地图里
+			if event.code not in self.event_sensor_map:
+				# self.logger.info(f"未知事件代码: {event.code}")
+				continue
+			# 获取该传感器
+			sensor_list = self.event_sensor_map[event.code]
+			# 执行回调函数
+			for sensor in sensor_list:
+				# self.logger.info(f"响应传感器: {sensor.name}")
+				sensor.event_handler(event)
+	
+	# def event_listener_loop(self):
+	# 	'''事件监听器'''
+	# 	while True:
+	# 		# 退出
+	# 		# if self.exit_event.is_set():
+	# 		# 	break
+	# 		# 事件监听
+	# 		self.event_listener()
+	
+	def start_event_listener_thread(self):
+		'''开启事件监听器线程'''
+		# 创建线程
+		self.event_listener_thread = EventListenerThread(self)
+		# 开启线程
+		self.event_listener_thread.start()
+	
+	def stop_event_listener_thread(self):
+		'''停止事件监听器线程'''
+		# 线程停止
+		self.event_listener_thread.stop()
--- src/utils/thread.py
+++ src/utils/thread.py
@@ -3,12 +3,27 @@ import threading
 
 class KillableThread(threading.Thread):
     '''可以被杀死的线程'''
-    def kill(self, consequences: bool = False) -> None:
+        
+    def kill(self) -> None:
         '''用非安全的手段杀死线程, 因此这个线程不能掌握重要资源'''
         # 获取_tstate_lock这个锁
+        # lock = self._tstate_lock
+        # if lock is not None and lock.locked():
+        #     # 锁释放
+        #     lock.release()
+        # self.join()
+        self._wait_for_tstate_lock()
+    
+    def _wait_for_tstate_lock(self, block=True, timeout=-1):
+        # Issue #18808: wait for the thread state to be gone.
+        # At the end of the thread's life, after all knowledge of the thread
+        # is removed from C data structures, C code releases our _tstate_lock.
+        # This method passes its arguments to _tstate_lock.acquire().
+        # If the lock is acquired, the C code is done, and self._stop() is
+        # called.  That sets ._is_stopped to True, and ._tstate_lock to None.
         lock = self._tstate_lock
-        
-        if lock is not None and lock.locked():
-            # 锁释放
+        if lock is None:  # already determined that the C code is done
+            assert self._is_stopped   # we see that we don't wait for anything here
+        elif lock.acquire(block, timeout):
             lock.release()
-        self.join()
+            self._stop()
